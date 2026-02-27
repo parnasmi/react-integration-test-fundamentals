406c8f3fc18a616a2fdd545d6405929c63c37edb
ilkhom_maksadkulov <ilkhom.maksadkulov@opentech.uz>
2026-02-27 11:38:04 +0500

    feat: Set up Mock Service Worker (MSW) for API mocking in Vitest tests and configure Vitest with tsconfig paths and test options.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
   test: {
     environment: "jsdom",
+    globals: true,
+    reporters: "verbose",
+    watch: true,
+    clearMocks: true,
+    // setupFiles: "./src/setupTests.ts",
   },
 });
